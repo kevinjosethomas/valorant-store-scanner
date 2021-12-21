b20c64517795ae0e40adb97fc3e566ac0ad2602a
Kevin Thomas <kevin.jt2007@gmail.com>
2021-12-21 13:14:01 -0800

    feat: added login auth flow

--- packages/api/src/app.js
+++ packages/api/src/app.js
@@ -1,7 +1,13 @@
+import dotenv from "dotenv";
 import fastify from "fastify";
 import { fileURLToPath } from "url";
-import static from "fastify-static";
+import _static from "fastify-static";
 import path, { dirname } from "path";
+
+import skins from "./plugins/skins.js";
+import middleware from "./plugins/middleware.js";
+
+import user_login from "./routes/user/login.js";
 
 dotenv.config();
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -10,7 +16,26 @@ const app = fastify({
   logger: true,
 });
 
-app.register(static, {
+app.setErrorHandler((error, req, res) => {
+  if (error.validation) {
+    return res.status(400).send({
+      success: false,
+      message: `Bad Request - ${error.validation[0].dataPath} ${error.validation[0].message}`,
+    });
+  }
+
+  return res.status(error.statusCode).send({
+    success: false,
+    message: `${error.statusCode} - ${error.message}`,
+  });
+});
+
+app.register(skins);
+app.register(middleware);
+
+app.register(user_login);
+
+app.register(_static, {
   root: path.join(__dirname, "../assets"),
   prefix: "/public/",
 });
@@ -19,3 +44,7 @@ const run = async () => {
   await app.listen(process.env.PORT);
   console.log(`Server listening on Port ${process.env.PORT}`);
 };
+
+run().catch((e) => {
+  console.log(e);
+});
---
+++ packages/api/src/routes/user/login.js
@@ -0,0 +1,170 @@
+import axios from "axios";
+
+const UserLoginSchema = {
+  body: {
+    type: "object",
+    required: ["username", "password", "region"],
+    properties: {
+      username: { type: "string", minLength: 3, maxLength: 16 },
+      password: { type: "string", minLenth: 8, maxLength: 128 },
+      region: {
+        type: "string",
+        enum: ["na", "eu", "kr", "ap"],
+      },
+    },
+  },
+};
+
+export default async function router(fastify) {
+  fastify.post("/user/login", { schema: { body: UserLoginSchema.body } }, async (req, res) => {
+    const { username, password, region } = req.body;
+
+    const [session, sessionError] = await CreateLoginSession();
+
+    if (sessionError) {
+      console.log(sessionError);
+      return res.code(424).send({
+        success: false,
+        message: "Failed Dependency - Failed to create a VALORANT login session",
+      });
+    }
+
+    const cookies = session.headers["set-cookie"].join("; ");
+
+    const [auth, authError] = await Login(username, password, cookies);
+    if (authError) {
+      if (authError.response.status === 401) {
+        return res.code(401).send({
+          success: false,
+          message: "Unauthorized - Invalid username or password provided",
+        });
+      } else {
+        return res.code(424).send({
+          success: false,
+          message: "Failed Dependency - Failed to login via the provided username and password",
+        });
+      }
+    }
+
+    const [token, tokenError] = FilterAccessToken(auth);
+
+    if (!token || tokenError) {
+      return res.code(500).send({
+        success: false,
+        message: "Internal Server Error - Failed to fetch access token",
+      });
+    }
+
+    const [entitlement, player] = await Promise.all([
+      FetchEntitlementToken(token),
+      FetchPlayerID(token),
+    ]);
+
+    if (entitlement[1]) {
+      return res.code(424).send({
+        success: false,
+        message: "Failed Dependency - Failed to fetch entitlement token",
+      });
+    }
+
+    if (player[1]) {
+      return res.code(424).send({
+        success: false,
+        message: "Failed Dependency - Failed to fetch player ID",
+      });
+    }
+
+    const entitlement_token = entitlement[0].data.entitlements_token;
+    const id = player[0].data.sub;
+
+    return res.code(200).send({
+      success: true,
+      payload: {
+        id,
+        entitlement_token,
+        access_token: token,
+      },
+    });
+  });
+}
+
+const CreateLoginSession = async () => {
+  try {
+    const response = await axios.post(`${process.env.RIOT_AUTH_URL}/api/v1/authorization`, {
+      nonce: "1",
+      client_id: "play-valorant-web-prod",
+      redirect_uri: "https://playvalorant.com/opt_in",
+      response_type: "token id_token",
+    });
+
+    return [response, null];
+  } catch (e) {
+    return [null, e];
+  }
+};
+
+const Login = async (username, password, cookies) => {
+  try {
+    const response = await axios.put(
+      `${process.env.RIOT_AUTH_URL}/api/v1/authorization`,
+      {
+        type: "auth",
+        username: username,
+        password: password,
+      },
+      {
+        headers: {
+          Cookie: cookies,
+        },
+      }
+    );
+
+    return [response, null];
+  } catch (e) {
+    return [null, e];
+  }
+};
+
+const FilterAccessToken = (raw) => {
+  try {
+    const token = raw.data.response.parameters.uri.match(
+      /access_token=((?:[a-zA-Z]|\d|\.|-|_)*).*id_token=((?:[a-zA-Z]|\d|\.|-|_)*).*expires_in=(\d*)/
+    )[1];
+
+    return [token, null];
+  } catch (e) {
+    return [null, e];
+  }
+};
+
+const FetchEntitlementToken = async (access_token) => {
+  try {
+    const response = await axios.post(
+      `${process.env.ENTITLEMENTS_AUTH_URL}/api/token/v1`,
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+      }
+    );
+
+    return [response, null];
+  } catch (e) {
+    return [null, e];
+  }
+};
+
+const FetchPlayerID = async (access_token) => {
+  try {
+    const response = await axios.get(`${process.env.RIOT_AUTH_URL}/userinfo`, {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    });
+
+    return [response, null];
+  } catch (e) {
+    return [null, e];
+  }
+};
