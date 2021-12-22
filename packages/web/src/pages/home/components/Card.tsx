2c033947fc6065d656544c1eb9b0919e0c4df80c
Kevin Thomas <kevin.jt2007@gmail.com>
2021-12-21 16:25:06 -0800

    feat: more changes

---
+++ packages/web/src/pages/home/components/Card.tsx
@@ -0,0 +1,20 @@
+type CardProps = {
+  id: string;
+  name: string;
+};
+
+function Card(props: CardProps): JSX.Element {
+  return (
+    <div className="skincard-outside relative w-96 h-48 border-[3px] border-white border-opacity-50 rounded-2xl bg-gradient-to-tr from-card-de1 to-card-de2">
+      <div className="skincard-inside flex items-center justify-center w-full h-full overflow-visible">
+        <img
+          alt={props.name}
+          className="absolute min-w-fit h-24 rotate-45"
+          src={`${process.env.NEXT_PUBLIC_API_URL}/public/skins/${props.id}.png`}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Card;
--- packages/web/src/pages/home/components/Storefront.tsx
+++ packages/web/src/pages/home/components/Storefront.tsx
@@ -1,14 +1,16 @@
 import { Key } from "react";
 
+import Card from "./Card";
+
 type StorefrontProps = {
   store: any;
 };
 
 function Storefront(props: StorefrontProps): JSX.Element {
   return (
-    <div className="grid grid-cols-4 gap-x-5">
+    <div className="grid grid-cols-4 gap-x-16">
       {props.store.skins.map((skin: Record<string, string>, index: Key) => (
-        <div key={index} className="w-96 h-96 bg-red-500"></div>
+        <Card key={index} {...skin} />
       ))}
     </div>
   );
